1523b99d4eef9d0d7396fec240143d694495e305
Hiroshi Adaniya <Hiroshi.Adaniya@icloud.com>
2023-02-18 13:28:04 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * This prints out a string of characters using puts
+ * Main entry point
+ * return always 0 (success)
+ */
+int main(void)
+{
+	puts("Programming is like a multilingual puzzle", \n);
+	return (0);
+}
