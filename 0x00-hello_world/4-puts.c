209ff91d854ba2dc4ebbb7761c5cb060db2a6e2a
Hiroshi Adaniya <Hiroshi.Adaniya@icloud.com>
2023-02-19 04:44:44 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * This prints out a string of characters using puts
- * Main entry point
- * return always 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
