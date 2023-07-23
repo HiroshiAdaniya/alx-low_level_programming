3f86f84321c423dba41b9eb36c60199f2b715f86
Hiroshi Adaniya <hiroshi.adaniya@icloud.com>
2023-07-23 13:48:17 +0200

    debugged 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,22 +8,23 @@ int print_last_digit(int n)
 {
 	int pos = 48;
 	int print_last_digit = 48;
+	int zero = 48;
 
 	if (n > pos)
 	{
 		print_last_digit = n % 10;
-		_putchar('0' + print_last_digit);
+		_putchar(zero + print_last_digit);
 	}
 	else if (n < pos)
 	{
 		pos = n * -1;
 		print_last_digit = pos % 10;
-		_putchar('0' + print_last_digit);
+		_putchar(zero + print_last_digit);
 	}
 	else
 	{
 		print_last_digit = 48;
-		_putchar('0' + print_last_digit);
+		_putchar(zero + print_last_digit);
 	}
 
 	return (print_last_digit);
