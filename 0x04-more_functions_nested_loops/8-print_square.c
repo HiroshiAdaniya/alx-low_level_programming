260c378c4fe048c486265cc9b98c933981a88cdb
Hiroshi Adaniya <hiroshi.adaniya@icloud.com>
2023-09-06 20:10:22 +0200

    added: 8-print_square.c main.h

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_square - Entry point
+ * @size: user input / stdin (0)
+ * Return: pound signs or newline
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
