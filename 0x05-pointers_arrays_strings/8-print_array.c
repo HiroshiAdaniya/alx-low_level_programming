b340db6f1fa352ba00486ed5c1ad614dad7735c1
Hiroshi Adaniya <hiroshi.adaniya@icloud.com>
2023-10-02 22:55:34 +0200

    debugged 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,13 +10,10 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n - 1; i++)
 	{
-		if (i == n - 1)
-		{
-			printf("%d\n", a[i]);
-			break;
-		}
-		printf("%d, ", *(a+i));
+		printf("%d, ", *(a + i));
 	}
+	printf("%d\n", *(a + i));
+
 }
