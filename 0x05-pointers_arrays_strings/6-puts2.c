16351aa19b190f2dfbcc5180a56b57a8550cecc7
Hiroshi Adaniya <hiroshi.adaniya@icloud.com>
2023-10-02 21:01:40 +0200

    debugged: 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,11 +6,16 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
-	while (str[i] != '\0')
+	int k = 0;
+	int i;
+
+	while (str[k] != '\0')
+	{
+		k++;
+	}
+	for (i = 0; i < k; i = i + 2)
 	{
 		_putchar(str[i]);
-		i = i + 2;
 	}
 	_putchar('\n');
 }
