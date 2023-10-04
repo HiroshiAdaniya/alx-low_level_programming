4a4c785d601c384c3647c6270007dbc0015e7e73
Hiroshi Adaniya <hiroshi.adaniya@icloud.com>
2023-10-04 15:55:38 +0200

    debugged 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,40 +8,12 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int j = 0;
-	int k = 0;
-	int l = 0;
-	int result = 0;
 
-	while (s1[l] != '\0')
-		l++;
-	while (s2[j] != '\0')
-		j++;
-
-	if (l > j)
-		k = l;
-	else if (j > l)
-		k = j;
-	else
-		k = l;
-	while (i <= k)
+	while (s1[i] == s2[i])
 	{
-		if (s1[i] > s2[i])
-		{
-			result = 15;
-			break;
-		}
-		else if (s1[i] < s2[i])
-		{
-			result = -15;
-			break;
-		}
-		else if (s1[i] == s2[i])
-		{
-			i++;
-		}
+		if (s1[i] == '\0')
+			return (0);
+		i++;
 	}
-	if (s1[l] == s2[j] && i == k )
-		result = 0;
-	return (result);
+	return (s1[i] - s2[i]);
 }
