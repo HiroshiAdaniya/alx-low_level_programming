088928bb46d358af5cad953dc4d09222494617f1
Hiroshi Adaniya <hiroshi.adaniya@icloud.com>
2023-10-04 20:11:15 +0200

    debugged: 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,25 +6,18 @@
  */
 char *cap_string(char *str)
 {
-	int i = 0;
-	int j = 0;
-	char sep[] = " \t\n,;.!?\"(){}";
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		while (str[i] != sep[j] && sep[j] != '\0')
-			j++;
-		if (str[i] == sep[j])
-		{
-			i++;
-			if (str[i] >= 97 && str[i] <= 122)
-				str[i] = str[i] - 32;
-		}
-		else if (str[i] != sep[j] && str[i] >= 97 && str[i] <= 122)
-			i++;
-		else
-			i++;
-		j = 0;
+		if (i == 0 && (str[i] >= 'a' && str[i] <= 'z'))
+			str[i] -= 32;
+		if ((str[i] >= 'a' && str[i] <= 'z')
+				&& !((str[i - 1] >= 'a' && str[i - 1] <= 'z')
+					|| (str[i - 1] >= 'A' && str[i - 1] <= 'Z'))
+				&& !(str[i - 1] >= '0' && str[i - 1] <= '9')
+				&& str[i - 1] != '-')
+			str[i] -= 32;
 	}
-	return (str);
+	return (str)
 }
