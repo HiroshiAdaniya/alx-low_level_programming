11781c5f6244f5e64eaedf118436c828a323c00d
Hiroshi Adaniya <hiroshi.adaniya@icloud.com>
2023-10-04 19:57:34 +0200

    added: 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,30 +7,24 @@
 char *cap_string(char *str)
 {
 	int i = 0;
+	int j = 0;
+	char sep[] = " \t\n,;.!?\"(){}";
 
 	while (str[i] != '\0')
 	{
-		if (str[i] == 9 || str[i] == 10 || str[i] == 32 || str[i] == 33 || str[i] == 34 || str[i] == 40 || str[i] == 41 || str[i] == 44 || str[i] == 46 || str[i] == 59 || str[i] == 63 || str[i] == 123 || str[i] == 125)
+		while (str[i] != sep[j] && sep[j] != '\0')
+			j++;
+		if (str[i] == sep[j])
 		{
 			i++;
-			if (str[i] >= 92 && str[i] <= 122)
-			{
+			if (str[i] >= 97 && str[i] <= 122)
 				str[i] = str[i] - 32;
-				i++;
-			}
-			else if (str[i] == 9 || str[i] == 10 || str[i] == 32 || str[i] == 33 || str[i] == 34 || str[i] == 40 || str[i] == 41 || str[i] == 44 || str[i] == 46 || str[i] == 59 || str[i] == 63 || str[i] == 123 || str[i] == 125)
-			{
-				i++;
-				if (str[i] >= 92 && str[i] <= 122)
-				{
-					str[i] = str[i] - 32;
-					i++;
-				}
-			}
 		}
+		else if (str[i] != sep[j] && str[i] >= 97 && str[i] <= 122)
+			i++;
 		else
 			i++;
-
+		j = 0;
 	}
 	return (str);
 }
