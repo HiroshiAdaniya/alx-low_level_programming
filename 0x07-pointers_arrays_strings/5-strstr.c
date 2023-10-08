d27ff32651ec5500ac85ca8bbb8563fd05582a6f
Hiroshi Adaniya <hiroshi.adaniya@icloud.com>
2023-10-08 20:00:35 +0200

    debugged: 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,13 +9,29 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i = 0;
 	int j = 0;
+	int k = 0;
+	int l = 0;
+
+	while (needle[l] != '\0')
+		l++;
 
 	while (haystack[i] != '\0')
 	{
 		while (needle[j] != '\0' && needle[j] == haystack[i])
 		{
-			return (&haystack[i]);
+			j++;
 			i++;
+		}
+		if (l == j)
+		{
+			l = 0;
+			k = i - j;
+			while (l <= j)
+			{
+				return (&haystack[k]);
+				k++;
+				l++;
+			}
 		}
 		j = 0;
 		i++;
