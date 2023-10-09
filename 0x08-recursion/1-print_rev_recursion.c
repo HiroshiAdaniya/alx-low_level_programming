4fe2f2df8ffc515d51eeabe79e321ee2512fe738
Hiroshi Adaniya <hiroshi.adaniya@icloud.com>
2023-10-09 16:50:36 +0200

    debugged 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,17 +1,14 @@
 #include "main.h"
 /**
- * _print_rev_string - prints a string in reverse
+ * _print_rev_recursion - prints a string in reverse
  * @s: user input string / stdin (0)
  * Return: Always 0 (Success)
  */
 void _print_rev_recursion(char *s)
 {
-
 	if (*s != '\0')
 	{
-		_print_rev_recursion(s+1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
-	else
-		_putchar('\n');
 }
