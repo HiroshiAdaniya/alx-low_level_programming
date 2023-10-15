ce1b4779991d395d8b0848cb1a7a7618f136cc8b
Hiroshi Adaniya <hiroshi.adaniya@icloud.com>
2023-10-15 17:05:28 +0200

    debugging: 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * *_strdup: returns pointer to memory, which is a copy of given string
- * *str: string / stdin (0)
+ * *_strdup - returns pointer to memory, which is a copy of given string
+ * @str: string / stdin (0)
  * Return: NULL if str is NULL or insufficient memory, else a string
  */
 char *_strdup(char *str)
@@ -11,18 +11,19 @@ char *_strdup(char *str)
 	unsigned int j = 0;
 	char *s;
 
+	if (str == NULL)
+		return (NULL);
+
 	while (str[i] != '\0')
 		i++;
-
+	i++;
 	s = malloc(sizeof(char) * i);
 
 	for (; j < i; j++)
 	{
 		s[j] = str[j];
 	}
-	if (str == NULL)
-		return (NULL);
-	else if (s == NULL)
+	if (s > str)
 		return (NULL);
 	return (s);
 
