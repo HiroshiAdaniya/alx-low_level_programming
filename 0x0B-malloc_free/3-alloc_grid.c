47d3dd019174cd187c1bf1ab96df708377861143
Hiroshi Adaniya <hiroshi.adaniya@icloud.com>
2023-10-16 17:58:37 +0200

    added: 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,17 +11,16 @@ int **alloc_grid(int width, int height)
 	int **i = 0;
 	int h, w;
 
-	w = 0;
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	i = (int **)malloc(sizeof(int *) * width);
+	i = malloc(sizeof(int *) * height);
 	if (i == NULL)
 	{
 		free(i);
 		return (NULL);
 	}
-	for (w = 0; w < width; w++)
-		i[w] = (int *)malloc(height * sizeof(int));
+	for (h = 0; h < height; h++)
+		i[h] = malloc(sizeof(int) * width);
 	if (i == NULL)
 	{
 		free(i);
@@ -30,7 +29,9 @@ int **alloc_grid(int width, int height)
 	for (h = 0; h < height; h++)
 	{
 		for (w = 0; w < width; w++)
+		{
 			i[h][w] = 0;
+		}
 	}
 	return (i);
 }
