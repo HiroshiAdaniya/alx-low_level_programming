aa502140ce15ae4b6ef5b48a11fad5bc52edaaf1
Hiroshi Adaniya <hiroshi.adaniya@icloud.com>
2023-10-16 11:53:30 +0200

    debugging: 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,12 +11,13 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j, k, l;
 	char *p;
 
-	if (s1 == NULL)
-		s1 = '\0';
-	if (s2 == NULL)
-		s2 = '\0';
+	k = 0;
 	i = 0;
 	j = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (s1[i] != '\0')
 		i++;
 	while (s2[j] != '\0')
