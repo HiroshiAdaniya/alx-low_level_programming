f18e5396eab4341498f58571818e76658ca2e8fb
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-01-11 22:32:17 +0200

    debugging: 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,32 +7,24 @@
 int _atoi(char *s)
 {
 	int i = 0;
-	int neg, pos = 0;
+	int j = 0;
 	int sign = 1;
 	unsigned int num = 0;
 
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
-			neg++;
-		else if (s[i] == '+')
-			pos++;
-		if (s[i] - '0' <= 9 && s[i] - '0' >= 0)
+			sign = sign * -1;
+		while (s[i] - '0' <= 9 && s[i] - '0' >= 0)
 		{
+			j = 1;	
 			num = num * 10 + s[i] - '0';
-			if (s[i + 1] != '\0' && !(s[i + 1] - '0' <= 9 && s[i + 1] - '0' >= 0))
-				break;
+			i++;
 		}
+		if (j == 1)
+			break;
 		i++;
 	}
 
-	if ((neg % 2 == 0 && pos % 2 == 0) || (pos == 1 && neg == 0))
-		sign = 1;
-	else if ((neg % 2 == 1 && pos % 2 == 1) || (neg == 1 && pos == 0))
-		sign = -1;
-	else if (neg == 0 && pos == 0)
-		sign  = 1;
-	else
-		sign = -1;
 	return (sign * num);
 }
