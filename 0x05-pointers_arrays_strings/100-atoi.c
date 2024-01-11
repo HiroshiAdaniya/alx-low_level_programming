cd9190afbf3f767634610107356c35460c2408b4
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-01-11 21:26:58 +0200

    added: 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,14 +6,31 @@
  */
 int _atoi(char *s)
 {
-	unsigned int i = 0;
-	
-	while (s[i] !- '\0')
+	int i = 0;
+	int neg, pos = 0;
+	int sign = 1;
+	int num = 0;
+
+	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
-			_putchar(s[i]);
-		if (s[i] >= 48 && s[i] <= 57)
-			_putchar[s[i]];
+			neg++;
+		else if (s[i] == '+')
+			pos++;
+		if (s[i] - '0' <= 9 && s[i] - '0' >= 0)
+		{
+			num = num * 10 + s[i] - '0';
+			if (s[i + 1] != '\0' && !(s[i + 1] - '0' <= 9 && s[i + 1] - '0' >= 0))
+				break;
+		}
+		i++;
 	}
-	return (0);	
+
+	if ((neg % 2 == 0 && pos % 2 == 0) || (pos == 1 && neg == 0))
+		sign = 1;
+	else if ((neg % 2 == 1 && pos % 2 == 1) || (neg == 1 && pos == 0))
+		sign = -1;
+	else
+		sign = -1;
+	return (sign * num);
 }
