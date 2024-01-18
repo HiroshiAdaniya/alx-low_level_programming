402bb13d368f78edd2a4102323626ecbb8d757ff
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-01-18 22:04:32 +0200

    debugging: 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	z = malloc(sizeof(unsigned int) * nmemb);
+	z = malloc(nmemb);
 	if (z == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb; i++)
