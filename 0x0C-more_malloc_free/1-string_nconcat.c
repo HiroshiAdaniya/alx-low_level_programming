3d45829a039b03629c855e78d89cc4fefb3908e3
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-01-18 22:31:37 +0200

    debugging: 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,35 +15,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	i = length(s1);
 	j = length(s2);
-	l = i + j + 1;
+	l = i + j;
 
-	string = malloc(sizeof(unsigned int) * l);
+	string = malloc(sizeof(char) * l + 1);
 	if (string == NULL)
 		return (NULL);
 
-	while (k < i)
-	{
+	for (k = 0; k < i; k++)
 		string[k] = s1[k];
-		k++;
-	}
-	i = 0;
+
 	if (n >= j)
 	{
-		while (i < j)
-		{
+		for (i = 0; i < j; i++, k++)
 			string[k] = s2[i];
-			k++;
-			i++;
-		}
 	}
 	else
 	{
-		while (i < n)
-		{
+		for (i = 0; i < n; i++, k++)
 			string[k] = s2[i];
-			k++;
-			i++;
-		}
 	}
 	string[k] = '\0';
 	return (string);
