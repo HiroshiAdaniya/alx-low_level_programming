20394124f616719dea726c80f14a10dc346bcd0e
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-01-19 18:12:18 +0200

    added: 100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _realloc - reallocates memory
+ * @ptr: void pointer
+ * @old_size: unsigned int
+ * @new_size: unsigned int
+ * Return: a pointer, else NULL
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *p;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+		return (p);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	p = malloc(new_size);
+	if (p == NULL)
+		return (NULL);
+	p = ptr;
+	free(ptr);
+	return (p);
+}
