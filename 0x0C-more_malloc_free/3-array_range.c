062adcdb736aa20b35efd3be28b96606e46d88ca
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-01-19 16:55:03 +0200

    debugging: 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -8,7 +8,7 @@
  */
 int *array_range(int min, int max)
 {
-	int *z, *p;
+	int *z;
 	int mem, i = 0;
 
 	if (min > max)
@@ -17,16 +17,16 @@ int *array_range(int min, int max)
 	mem = max - min;
 	z = malloc(sizeof(int) * mem);
 	if (z == NULL)
+		return (NULL);
+	for (i = 0; min < max; i++, min++)
+		z[i] = min;
+	mem = mem + 1;
+	z = realloc(z, sizeof(int) * mem);
+	if (z == NULL)
 	{
 		free(z);
 		return (NULL);
 	}
-	for (i = 0; min < max; i++, min++)
-		z[i] = min;
-	mem = mem + 1;
-	p = realloc(z, sizeof(int) * mem);
-	if (p == NULL)
-		return (NULL);
-	p[i] = max;
-	return (p);
+	z[i] = max;
+	return (z);
 }
