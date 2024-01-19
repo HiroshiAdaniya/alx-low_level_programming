d040ebb95930a866228551ef6d01aa9260f589e5
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-01-19 19:09:15 +0200

    debugging: 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,17 +10,17 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *p;
-	
+
 	if (new_size == old_size)
 		return (ptr);
-	if (ptr == NULL)
+	else if (ptr == NULL)
 	{
 		ptr = malloc(new_size);
 		if (ptr == NULL)
 			return (NULL);
 		return (ptr);
 	}
-	if (new_size == 0 && ptr != NULL)
+	else if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
