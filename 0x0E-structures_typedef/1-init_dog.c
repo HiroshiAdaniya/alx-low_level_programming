fb7d6b5bbd0704c64fdafce01f6055dae047a6c4
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-01-22 22:51:46 +0200

    debugging: 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,7 +10,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
 	d->owner = owner;
+	d->age = age;
+	d->name = name;
 }
