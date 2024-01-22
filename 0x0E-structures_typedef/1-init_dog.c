c862949d07013a20460159e0b1ee8c640dec638e
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-01-22 20:29:09 +0200

    debugging: 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,27 +9,8 @@
  * Return: always 0 (Success)
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
-{
-	int i = 0;
-
-	while (name[i] != '\0')
-		i++;
-	d->name = malloc(i + 1);
-	while (name[i] != '\0')
-	{
-		d->name[i] = name[i];
-		i++;
-	}
-	d->name[i] = '\0';
+{	
 	d->age = age;
-	i = 0;
-	while (owner[i] != '\0')
-		i++;
-	d->owner = malloc(i + 1);
-	while (owner[i] != '\0')
-	{
-		d->owner[i] = owner[i];
-		i++;
-	}
-	d->owner[i] = '\0';
+	d->name = name;
+	d->owner = owner;
 }
