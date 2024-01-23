e2aaed8b012c87432dcceeeb7b8a5aec6573335b
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-01-23 16:35:22 +0200

    debugging: 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,7 @@
 #include "dog.h"
 #include <stdlib.h>
+int length(char *);
+char *copys(char *, char *);
 /**
  * new_dog - stores data for a new dog
  * @name: pointer to a string of char
@@ -10,24 +12,20 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *pet;
-	int i, j, k = 0;
+	int i, j = 0;
 
 	pet = malloc(sizeof(dog_t));
 	if (pet == NULL)
 		return (NULL);
-
-	while (name[i] != '\0')
-		i++;
-	while (owner[j] != '\0')
-		j++;
-
-	pet->name = malloc(sizeof(char) * (i + 1));
+	i = 1 + length(name);
+	j = 1 + length(owner);
+	pet->name = malloc(sizeof(char) * i);
 	if (pet->name == NULL)
 	{
 		free(pet);
 		return (NULL);
 	}
-	pet->owner = malloc(sizeof(char) * (j + 1));
+	pet->owner = malloc(sizeof(char) * j);
 	if (pet->owner == NULL)
 	{
 		free(pet->name);
@@ -35,12 +33,39 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
+	pet->name = copys(pet->name, name);
+	pet->owner = copys(pet->owner, owner);
 	pet->age = age;
-	for (k = 0; k < i; k++)
-		pet->name[k] = name[k];
-	pet->name[k] = '\0';
-	for (k = 0; k < j; k++)
-		pet->owner[k] = owner[k];
-	pet->owner[k] = '\0';
 	return (pet);
+}
+/**
+ * length - finds the length of a string
+ * @z: pointer to a string
+ * Return: int
+ */
+int length(char *z)
+{
+	int i = 0;
+
+	while (z[i] != '\0')
+		i++;
+
+	return (i);
+}
+/**
+ * copys - copy a string to a new space in memory
+ * @a: a pointer to a struct
+ * @b: the string to copy
+ * Return: a pointer to the new memory location
+ */
+char *copys(char *a, char *b)
+{
+	int i = 0;
+
+	for (i = 0; b[i] != '\0'; i++)
+		a[i] = b[i];
+	a[i] = '\0';
+
+	return (a);
+
 }
