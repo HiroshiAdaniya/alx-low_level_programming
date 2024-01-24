2393ff63cc347b922ac712a689952b0ac52f4f5e
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-01-24 23:42:31 +0200

    debugging: 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,7 +11,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i = 0;
 
 	if (array == NULL || cmp == NULL)
-		exit(EXIT_FAILURE);
+		return (-1);
 	if (size > 0)
 		for (i = 0; i < size; i++)
 		{
