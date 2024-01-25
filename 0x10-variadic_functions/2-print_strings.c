b2fccaf903b72f4610be9e3a30417c1cf4940ee0
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-01-25 20:15:22 +0200

    debugging: 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,14 +12,15 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list  z;
 
 	va_start(z, n);
-
+	if (separator == NULL)
+		separator = "";
 	for (i = 0; i < n; i++)
 	{
 		string = va_arg(z, char *);
 		if (*string == '\0')
 			string = "(nil)";
 		printf("%s", string);
-		if ((i < n - 1) && separator != NULL)
+		if ((i < n - 1))
 			printf("%s", separator);
 	}
 	printf("\n");
