b47b2f5bdac958b062d1bf6936b790a58cc5abca
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-01-25 20:10:32 +0200

    debugging: 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,8 +13,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(z, n);
 
-	if (n == 0)
-		return;
 	if (separator == NULL)
 		separator = "";
 	for (i = 0; i < n; i++)
