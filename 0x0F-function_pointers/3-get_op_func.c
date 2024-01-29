a32e8932115e44c8f72edc2e95f82af39ae11341
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-01-29 11:11:43 +0200

    debugging code

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -14,10 +14,9 @@ int (*get_op_func(char *s))(int, int)
 	{"%", op_mod},
 	{NULL, NULL}
 	};
-	int i;
+	int i = 0;
 
-	i = 0;
-	while (ops[i].op != NULL)
+	while (ops[i].op != s)
 	{
 		if (*ops[i] == *s)
 			return (ops[i].f);
