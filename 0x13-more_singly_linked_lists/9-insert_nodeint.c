d8dd6c94431bf8ae952c859cdbb98b2a0f23138c
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-02-06 18:30:30 +0200

    added: 9-insert_nodeint.c

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - adds a new node at a given position
+ * @head: pointer to a linked list
+ * @idx: the point where the node should be inserted
+ * @n: the data to be added
+ * Return: a pointer to the new node, else NULL om failure
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *node, *temp;
+	unsigned int i = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	temp = *head;
+
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
+	while (temp != NULL)
+	{
+		if (i == idx  && temp != NULL)
+		{
+			node->next = temp;
+			node->n = n;
+			temp = node;
+			break;
+		}
+		else if (i >= idx && temp == NULL)
+		{
+			free(node);
+			return (NULL);
+		}
+		temp = temp->next;
+		i++;
+	}
+	return (node);
+}
