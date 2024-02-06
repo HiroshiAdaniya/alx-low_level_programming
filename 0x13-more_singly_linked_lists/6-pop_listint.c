8a5c73ff1a66078f99133aa85a77981cde81c5d5
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-02-06 16:39:51 +0200

    added: 6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * pop_listint - deletes the head node and returns the data
+ * @head: pointer to a linked list
+ * Return: Node data (an int) / else 0
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *node;
+	int i = 0;
+
+	if (head == NULL)
+		return (0);
+	i = (*head)->n;
+	if (*head != NULL)
+	{
+		node = (*head)->next;
+		free(*head);
+		*head = node;
+	}
+	return (i);
+}
