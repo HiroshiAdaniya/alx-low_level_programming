e2e82f3bed1618ee955c415556e1ceb564a1157a
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-02-06 18:38:50 +0200

    debugging: 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,13 +28,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			break;
 		}
 		else if (i >= idx && *head == NULL)
-		{
-			free (node);
 			return (NULL);
-		}
 		temp = temp->next;
 		i++;
 	}
-	free(node);
 	return (node);
 }
