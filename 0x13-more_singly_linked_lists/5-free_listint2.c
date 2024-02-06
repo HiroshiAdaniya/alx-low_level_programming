7d38d1345025d585a24e075ff25779f36f6d662f
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-02-06 13:55:28 +0200

    added: 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,9 +8,9 @@ void free_listint2(listint_t **head)
 {
 	listint_t *node;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return;
-	while ((*head)->next != NULL)
+	while (*head != NULL)
 	{
 		node = (*head)->next;
 		free(*head);
