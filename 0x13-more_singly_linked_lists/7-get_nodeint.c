51c2fdacb71e294f0fdc9421385285de31eb3f25
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-02-06 17:40:14 +0200

    debugging: 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,24 +7,18 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *node;
 	unsigned int i = 0;
 
 	if (head == NULL)
 		return (NULL);
-	while (head != NULL && !(i > index))
+	while (head != NULL)
 	{
 		if (i == index && head != NULL)
-		{
-			node = head;
 			break;
-		}
-		else if (i == index && head == NULL)
+		else if (i >= index && head == NULL)
 			return (NULL);
 		head = head->next;
 		i++;
 	}
-	if (i > index)
-		return (NULL);
-	return (node);
+	return (head);
 }
