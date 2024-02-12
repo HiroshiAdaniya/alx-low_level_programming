66606006455a4e31c0822dabd09ce51378992e49
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-02-13 00:07:48 +0200

    debugging

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,17 +12,16 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	filep = open(filename, O_CREAT, S_IRUSR | S_IWUSR /*or 0600*/);
-	if (filep == -1)
-		return (-1);
-	close(filep);
 	fp = fopen(filename, "w");
 	if (fp == NULL)
 		return (-1);
 	j = fputs(text_content, fp);
 	if (j == -1)
 		return (-1);
-
 	fclose(fp);
+	filep = open(filename, O_CREAT, S_IRUSR | S_IWUSR /*or 0600*/);
+	if (filep == -1)
+		return (-1);
+	close(filep);
 	return (1);
 }
