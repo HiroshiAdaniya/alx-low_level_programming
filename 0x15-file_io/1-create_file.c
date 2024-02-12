2b6abfd579ebe9053a3f02c5a3c824f318d6ba71
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-02-13 00:35:18 +0200

    debugging:1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,10 +15,13 @@ int create_file(const char *filename, char *text_content)
 	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fp == -1)
 		return (-1);
-	while (text_content[i] != '\0')
-		i++;
+	if (text_content != NULL)
+	{
+		while (text_content[i] != '\0')
+			i++;
+	}
 	j = write(fp, text_content, i);
-	if (j == -1)
+	if (j == -1 || fp == -1)
 		return (-1);
 	close(fp);
 	return (1);
