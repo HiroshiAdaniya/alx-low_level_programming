e6379a02a6c6b9560833863cf1298c92e539af18
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-02-13 10:38:33 +0200

    added: 2-append_text_to_file.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * append_text_to_file - appends text to the end of a file
+ * @filename: name of file
+ * @text_content: text to append
+ * Return: 1 on success, else -1  on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int i, j = 0;
+	int fp;
+
+	if (filename == NULL)
+		return (-1);
+	fp = open(filename, O_RDWR | O_APPEND);
+	if (fp == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[i] != '\0')
+			i++;
+	}
+	j = write(fp, text_content, i);
+	if (j == -1 || fp == -1)
+		return (2);
+
+	return (1);
+}
