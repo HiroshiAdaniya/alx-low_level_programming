73a328d1be5fde33e2190a10516bdb60dbf4c12f
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-02-13 11:40:25 +0200

    debugging: 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,10 +19,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		while (text_content[i] != '\0')
 			i++;
-	}
-	j = write(fp, text_content, i);
-	if (j == -1 || fp == -1)
+		j = write(fp, text_content, i);
+		if (j == -1 || fp == -1)
 		return (-1);
-
+	}
 	return (1);
 }
