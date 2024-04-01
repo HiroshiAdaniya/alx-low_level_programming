3092299dc4ac91c0ba8cb90172d0d8d88cc5f3db
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-04-01 16:38:08 +0200

    added: 100-first.c, updated lists.h

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * print - prints a string / executes function for main
+ *
+ * Return: Nothing / void
+ */
+int __attribute__ ((constructor)) print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+	return (0);
+}
