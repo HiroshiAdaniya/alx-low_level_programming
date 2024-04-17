18b9287630e2a3170c7357d09ceb653654472024
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-04-17 17:40:58 +0200

    debugging: 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-int binary(unsigned long int n);
 /**
  * get_bit - Returns the value of a bit at a given index
  * @n: Unsigned long int / the number that holds the value
@@ -12,11 +11,11 @@ int get_bit(unsigned long int n, unsigned int index)
 	int bit = 1;
 
 	bit = bit << index;
-	if (bit == '\0')
-		return (-1);
 	if (n & bit)
 		bit = 1;
-	else
+	else if (n | bit)
 		bit = 0;
+	else
+		bit = -1;
 	return (bit);
 }
