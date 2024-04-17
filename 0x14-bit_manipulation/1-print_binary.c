8846556c8908f8da8101ccc1ee9b265aa8f888d1
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-04-17 21:16:40 +0200

    debugging: 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,41 +6,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int i = 0;
-
-	if (n == 0)
-		_putchar(48);
-	while (powers(2, i) <= n)
-		i++;
-	while (i)
-	{
-		i--;
-		if (n < powers(2, i))
-			_putchar(48);
-		else if (n > powers(2, i) || n == powers(2, i))
-		{
-			_putchar(49);
-			n = n - powers(2, i);
-		}
-		if (i == 0)
-			break;
-	}
-}
-/**
- * powers - Finds the power of 2
- * @base: The base value
- * @pow: The power to raise the base by
- * Return: an int
- */
-unsigned long int powers(unsigned long int base, unsigned long int pow)
-{
-	unsigned long int sum = 1;
-
-	while (pow > 0)
-	{
-		sum = sum * base;
-		pow--;
-	}
-
-	return (sum);
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
