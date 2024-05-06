8b40adfbaf9d149a54ff2786baf1506cc1b1835f
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-05-06 16:41:12 +0200

    debugging: 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,70 +1,91 @@
 #include "main.h"
-void error(int, char *);
+void file_to_error(char *);
+void file_from_error(char *);
+void close_error(int);
+void transfer_data(int, int, char *, char *);
 /**
- * main - copies the content of one file to another
+ * main - Copies context from a file to another
  * @argc: argument counter
- * @argv: argument vector / pointer to array of string
- * Return: 0 on success, else terminate with exit codes
+ * @argv: argument vector
+ * Return: an int
  */
 int main(int argc, char *argv[])
 {
-	int fileto, filefrom, j;
-	ssize_t i = 1024;
-	char buffer[1024];
+	int file_from, file_to;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		write(STDERR_FILENO, "Usage: cp file_from file_to\n", 29);
 		exit(97);
 	}
 
-	filefrom = open(argv[1], O_RDONLY);
-	if (filefrom == -1)
-		error(98, argv[1]);
-	fileto = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
-	if (fileto == -1)
-		error(99, argv[2]);
+	file_to = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
+	| S_IRGRP | S_IWGRP | S_IROTH);
+	if (file_to == -1)
+		file_to_error(argv[2]);
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+		file_from_error(argv[1]);
 
-	while (i == 1024)
-	{
-		i = read(filefrom, buffer, 1024);
-		if (i == -1)
-			error(98, argv[1]);
-		j = write(fileto, buffer, i);
-		if (j == -1)
-			error(99, argv[2]);
-	}
-	j = close(filefrom);
-	if (j == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", filefrom);
-		exit(100);
-	}
-	j = close(fileto);
-	if (j == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileto);
-		exit(100);
-	}
+	transfer_data(file_from, file_to, argv[1], argv[2]);
+
+	if (close(file_from) == -1)
+		close_error(file_from);
+	if (close(file_to) == -1)
+		close_error(file_to);
 
 	return (0);
 }
 /**
- * error - prints an error message to the POSIX standard error
- * @a: an exit code / int
- * @string: pointer to a string
- * Return: Nothing / void
+ * file_from_error - prints an error message related to file_from
+ * @file: name of file
+ * Return: Nothing / void / exits with code
  */
-void error(int a, char *string)
+void file_from_error(char *file)
 {
-	if (a == 98)
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+	exit(98);
+
+}
+/**
+ * transfer_data - copies the text from a file to another
+ * @file_from: a file descriptor
+ * @file_to: a file descriptor
+ * @f1: name of file from
+ * @f2: name of file to
+ * Retun: Nothing / void
+ */
+void transfer_data(int file_from, int file_to, char *f1, char *f2)
+{
+	char buffer[1024];
+	int i = 0;
+
+	while ((i = read(file_from, buffer, 1024)) != 0 && i != EOF)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", string);
-		exit(98);
+		if (i == -1)
+			file_from_error(f1);
+		i =  write(file_to, buffer, i);
+		if (i == -1)
+			file_to_error(f2);
 	}
-	if (a == 99)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", string);
-		exit(99);
-	}
+}
+/**
+ * file_to_error - prints an error message related to file_t
+ * @file: name of file
+ * Return: Nothing / void / exits with code
+ */
+void file_to_error(char *file)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+	exit(99);
+}
+/**
+ * close_error - prints an error message if close fails
+ * @fp: file descriptor
+ * Return: Nothing / void / exits with code
+ */
+void close_error(int fp)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fp);
+	exit(100);
 }
