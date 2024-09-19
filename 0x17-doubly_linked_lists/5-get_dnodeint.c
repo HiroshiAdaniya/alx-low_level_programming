f357af6b63ea17041ac4c6758bc109cff0b091d8
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-09-19 23:01:25 +0200

    added: 5-get_dnodeint.c, updated lists.h

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - Finds the nth node of a linked list
+ * @head: Head of a linked list
+ * @index: Index of the node to locate (int)
+ * Return: Nth node of linked list, else NULL
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (head != NULL && i <= index)
+	{
+		if (i == index)
+			break;
+		else if (i > index)
+			return (NULL);
+		i++;
+		head = head->next;
+	}
+
+	return (head);
+}
