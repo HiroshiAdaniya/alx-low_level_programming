e4bcf141ab08532183aa50e66ebed0927fcc6495
Hiroshi Adaniya <Hiroshi.adaniya@icloud.com>
2024-09-19 20:53:18 +0200

    debugging: 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,17 +14,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	newnode->n = n;
 	newnode->prev = NULL;
+	newnode->next = *head;
 
-	if (*head == NULL)
-	{
-		newnode->next = NULL;
-		*head = newnode;
-	}
-	else
-	{
-		newnode->next = *head;
-		*head = newnode;
-	}
+	if (*head != NULL)
+		(*head)->prev = newnode;
+	*head = newnode;
 
 	return (newnode);
 }
